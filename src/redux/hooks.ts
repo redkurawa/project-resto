7bc6442defa9690d6474a2bcc00216c51de02927
adi <adisudana@gmail.com>
2025-09-15 12:20:38 +0700

    add redux di menutype detail dan fix typo di hooks.ts.tsx

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,7 +1,11 @@
-// hooks.ts
+// hooks.ts // file ini belum dipakai langsung tulis di detail2.tsx
 import { useSelector, useDispatch } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
 
+// export const useMenuSelector = () =>
+//   useSelector((state: RootState) => state.menu.menus);
+// export const useAppDispatch = () => useDispatch<AppDispatch>();
+
 export const useMenuSelector = () =>
-  useSelector((state: RootState) => state.menu.menus);
+  useSelector((state: RootState) => state.menus.items);
 export const useAppDispatch = () => useDispatch<AppDispatch>();
