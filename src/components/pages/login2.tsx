a810cd301383e4555371cbb0a0e74cd02e06c673
adi <adisudana@gmail.com>
2025-09-16 13:19:54 +0700

    add animated when login

--- src/components/pages/login2.tsx
+++ src/components/pages/login2.tsx
@@ -6,9 +6,12 @@ import { loginUser } from '@/redux/auth-slice';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import React from 'react';
 import { useNavigate } from 'react-router';
+import { Loader2 } from 'lucide-react';
 
 export const Login2 = () => {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
   const [password, setPasswd] = useState('');
   // const [status, setStatus] = useState<string | null>(null);
   const navigate = useNavigate();
@@ -40,54 +43,126 @@ export const Login2 = () => {
         />
       </div>
       <div className='flex-center mx-auto min-h-screen w-full basis-1/2 lg:min-h-0'>
-        <Tabs
-          defaultValue='signIn'
-          className='w-[400px] rounded-2xl border p-2'
-        >
-          <TabsList>
-            <TabsTrigger value='signIn'>Sign In2</TabsTrigger>
-            <TabsTrigger value='signUp'>Sign Up2</TabsTrigger>
-          </TabsList>
-          <TabsContent value='signIn'>
-            <form onSubmit={handleSubmit}>
-              <Input
-                id='email'
-                placeholder='Email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className='my-2'
-              />
-              <Input
-                id='password'
-                placeholder='Password'
-                value={password}
-                onChange={(e) => setPasswd(e.target.value)}
-                className='my-2'
-              />
-              <Button className='w-full' type='submit' disabled={isLoading}>
-                {isLoading ? 'Loading...' : 'Login'}
-              </Button>
-              {/* {status && <p>{status}</p>} */}
-              {/* {status === 'Login failed' && (
+        <div className=''>
+          <div className='mb-5 flex items-center'>
+            <img src='/icons/logo.svg' alt='logo' />
+            <span className={`ml-2 text-[32px] font-extrabold text-black`}>
+              Foody
+            </span>
+          </div>
+          <h1 className='text-[28px] font-extrabold'> Welcome Back</h1>
+          <p className='mb-5 text-[16px] font-medium'>
+            Good to see you again! Let's eat
+          </p>
+          <Tabs defaultValue='signIn' className='w-[400px] rounded-2xl p-2'>
+            <TabsList className='w-full'>
+              <TabsTrigger value='signIn'>Sign In</TabsTrigger>
+              <TabsTrigger value='signUp'>Sign Up</TabsTrigger>
+            </TabsList>
+            <TabsContent value='signIn'>
+              <form onSubmit={handleSubmit}>
+                <Input
+                  id='email'
+                  placeholder='Email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className='my-2'
+                />
+                <Input
+                  id='password'
+                  placeholder='Password'
+                  value={password}
+                  onChange={(e) => setPasswd(e.target.value)}
+                  className='my-2'
+                />
+                <Button
+                  className='h-12 w-full rounded-full py-2'
+                  type='submit'
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <span className='flex items-center gap-2'>
+                      <Loader2 className='size-8 animate-spin text-red-100' />
+                      Loading...
+                    </span>
+                  ) : (
+                    'Login'
+                  )}
+                </Button>
+                {/* {status && <p>{status}</p>} */}
+                {/* {status === 'Login failed' && (
                 <p className='text-red-500'>Email atau password salah</p>
               )} */}
-              {/* {status?.startsWith('Login failed') && (
+                {/* {status?.startsWith('Login failed') && (
                 <p className='text-red-500'>Gagal login: {status}</p>
               )} */}
-            </form>
-          </TabsContent>
-          <TabsContent value='signUp'>Change your password here.</TabsContent>
-          {user && (
-            <div>
-              <h3>Welcome, {user.name}!</h3>
-              <p>Email: {user.email}</p>
-              <p className='break-all whitespace-normal'>token: {token}</p>
-            </div>
-          )}
-        </Tabs>
+              </form>
+            </TabsContent>
+            <TabsContent value='signUp'>
+              <form onSubmit={handleSubmit}>
+                <Input
+                  id='name'
+                  placeholder='Name'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className='my-2'
+                />
+                <Input
+                  id='email'
+                  placeholder='Email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className='my-2'
+                />
+                <Input
+                  id='phone'
+                  placeholder='Number Phone'
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  className='my-2'
+                />
+                <Input
+                  id='password'
+                  placeholder='Password'
+                  value={password}
+                  onChange={(e) => setPasswd(e.target.value)}
+                  className='my-2'
+                />
+                <Input
+                  id='password'
+                  placeholder='Password'
+                  value={password}
+                  onChange={(e) => setPasswd(e.target.value)}
+                  className='my-2'
+                />
+                <Button
+                  className='h-12 w-full rounded-full py-2'
+                  type='submit'
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <span className='flex items-center gap-2'>
+                      <Loader2 className='size-8 animate-spin text-red-100' />
+                      Register ...
+                    </span>
+                  ) : (
+                    'Register'
+                  )}
+                </Button>
+              </form>
+            </TabsContent>
+            {user && (
+              <div>
+                <h3>Welcome, {user.name}!</h3>
+                <p>Email: {user.email}</p>
+                <p className='break-all whitespace-normal'>token: {token}</p>
+              </div>
+            )}
+          </Tabs>
+          {/* {isLoading && <p>Loading...</p>} */}
+          {error && <p>Error: {error.message}</p>}
+        </div>
       </div>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
     </div>
   );
 };
